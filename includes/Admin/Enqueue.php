93c760f96ee1c45944089d4e771ead038c9780af
adnan159 <mac@MACs-Mac-mini.local>
2023-02-13 12:46:39 +0600

    Load js in specific page

--- includes/Admin/Enqueue.php
+++ includes/Admin/Enqueue.php
@@ -5,17 +5,19 @@ namespace AsCode\PostManager\Admin;
 class Enqueue {
 
     public function __construct() {
-        add_action( 'admin_enqueue_scripts', [$this, 'admin_script'] );
+        add_action( 'admin_enqueue_scripts', [$this, 'admin_script'], 10, 1 );
 
     }
 
     /**
      * Load script
      *
-     * @param [type] $page
+     * @param $page
      * @return void
      */
-    public function admin_script() {
-        wp_enqueue_script( 'asc_post_manager_dashboard', ASC_POST_MANAGER_ASSETS .'/admin/js/dashboard.js' );
+    public function admin_script( $page ) {
+        if( $page === 'toplevel_page_asc-post-manager' ) {
+            wp_enqueue_script( 'asc_post_manager_dashboard', ASC_POST_MANAGER_ASSETS .'/admin/js/dashboard.js' );
+        }
     }
 }
